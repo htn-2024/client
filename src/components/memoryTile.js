446f80be9bcb18391cf5a6497ab318a710243ee5
michelle-wangg <wang.michelle.mw@gmail.com>
2024-09-14 23:53:35 -0400

    other styling

--- src/components/memoryTile.js
+++ src/components/memoryTile.js
@@ -77,15 +77,19 @@ const styles = {
     cursor: 'pointer',
     borderRadius: '4px',
   },
+  borderRadius: '4px',
+  color: '#CB785C',
   dropdownItemRed: {
     padding: '4px 12px 8px 12px',
     width: '100%',
     border: 'none',
+    borderRadius: '4px',
     backgroundColor: '#fff',
     textAlign: 'left',
     cursor: 'pointer',
-    borderRadius: '4px',
-    color: '#CB785C',
+    '&:hover': {
+      backgroundColor: '#e9e9e9',
+    },
   },
   content: {
     marginTop: '-24px',
---
+++ src/pages/loginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+
+  return (
+    <div>
+      Page for google auth
+      <button onClick={() => {window.location.href = "/gallery";}}>Go to gallery</button>
+    </div>
+  );
+};
+
+export default LoginPage;
