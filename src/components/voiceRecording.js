30b30375a03631539f4000a147840f08950b90f8
alvinkam <alvin.kam.33@gmail.com>
2024-09-15 04:23:27 -0400

    change media recorder

--- src/components/voiceRecording.js
+++ src/components/voiceRecording.js
@@ -1,64 +1,22 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import { ReactMediaRecorder } from 'react-media-recorder';
 import './voiceRecording.css';
 
 const VoiceRecording = ({ setAudioBlob }) => {
-  const [isRecording, setIsRecording] = useState(false);
-
-  const mediaRecorderRef = useRef(null);
-  const audioChunksRef = useRef([]);
-  const audioRef = useRef(null);
-
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorderRef.current = new MediaRecorder(stream);
-      console.log(mediaRecorderRef.current);
-      audioChunksRef.current = [];
-
-      mediaRecorderRef.current.ondataavailable = (event) => {
-        audioChunksRef.current.push(event.data);
-      };
-
-      mediaRecorderRef.current.onstop = () => {
-        const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
-        setAudioBlob(audioBlob);
-        const audioURL = window.URL.createObjectURL(audioBlob);
-        if (audioRef.current) {
-          audioRef.current.src = audioURL;
-        }
-      };
-
-      mediaRecorderRef.current.start();
-      setIsRecording(true);
-      console.log('Recording started');
-    } catch (error) {
-      console.error('Error accessing the microphone', error);
-    }
-  };
-
-  const stopRecording = () => {
-    if (mediaRecorderRef.current && mediaRecorderRef.current.state !== 'inactive') {
-      mediaRecorderRef.current.stop();
-      setIsRecording(false);
-      console.log('Recording stopped');
-    }
-  };
-
-  const handleVoiceRecording = () => {
-    if (!isRecording) {
-      startRecording();
-    } else {
-      stopRecording();
-    }
-  };
-
   return (
-    <div className="recording">
-      <button className="record-button" type="button" onClick={handleVoiceRecording}>
-        {isRecording ? 'Stop Recording' : 'Record Voice'}
-      </button>
-      <audio ref={audioRef} controls/>
-    </div>
+    <ReactMediaRecorder
+      video={false}
+      audio={true}
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+        <div className="recording">
+          <button className="record-button" type="button" onClick={status === 'recording' ? stopRecording : startRecording}>
+            {status === 'recording' ? 'Stop Recording' : 'Record Voice'}
+          </button>
+          <audio src={mediaBlobUrl} controls />
+        </div>
+      )}
+      onStop={(_, blob) => setAudioBlob(blob)}
+    />
   )
 };
 
